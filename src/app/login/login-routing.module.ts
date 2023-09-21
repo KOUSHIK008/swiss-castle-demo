1e4bbfe161097f4a78e95503e331cd6de8c49b69
KOUSHIK008 <koushik.anumasa@snaddevelopers.com>
2023-09-21 18:51:17 +0530

    latest commit-(21-09-2023)

--- src/app/login/login-routing.module.ts
+++ src/app/login/login-routing.module.ts
@@ -8,16 +8,20 @@ import { SplashComponent } from './splash/splash.component';
 import { SelectOrderTypeComponent } from './select-order-type/select-order-type.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { Page2Component } from './page2/page2.component';
+import { Page3Component } from './page3/page3.component';
+import { Page4Component } from './page4/page4.component';
 
 const routes: Routes = [
-  { path: '', component: SplashComponent },
-  { path: 'getstarted', component: GetStartedPageComponent },
-  { path: 'signup', component: SignupComponent },
-  { path: 'otp', component: OtpComponent },
-  { path: 'details', component: DetailsComponent },
-  { path: 'order', component: SelectOrderTypeComponent },
-  { path: 'mainpage', component: MainPageComponent },
-  { path: 'page2', component: Page2Component },
+  { path: '', component: SplashComponent},
+  { path: 'getstarted', component: GetStartedPageComponent},
+  { path: 'signup', component: SignupComponent},
+  { path: 'otp', component: OtpComponent},
+  { path: 'details', component: DetailsComponent},
+  { path: 'order', component: SelectOrderTypeComponent},
+  { path: 'mainpage', component: MainPageComponent},
+  { path: 'page2', component: Page2Component},
+  { path: 'page3', component: Page3Component},
+  { path: 'page4', component: Page4Component},
 ];
 
 @NgModule({
